3ba3936ad4955dfe0a9808fe067b7e03c1f54ee2
ianto3 <33053514+ianto3@users.noreply.github.com>
2021-03-20 00:49:35 +0100

    styled with Bulma and refactored

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Score Keeper
 
 A simple application to keep tabs on the score in a 2 player game.
+
+Bulma was used as a framework for the project.
--- src/app.js
+++ src/app.js
@@ -1,58 +1,89 @@
 
-const player1btn = document.querySelector("#player1add");
-const player2btn = document.querySelector("#player2add");
-const resetbtn = document.querySelector("#reset");
-const player1Display = document.querySelector("#score1Display");
-const player2Display = document.querySelector("#score2Display");
-const victoryCond = document.querySelector("#playto");
+const player1 = {
+    button: document.querySelector("#player1add"),
+    color: "has-text-success",
+    display: document.querySelector("#score1Display"),
+    name: "Player One",
+    score: 0,
+};
 
-let player1Score = 0;
-let player2Score = 0;
+const player2 = {
+    button: document.querySelector("#player2add"),
+    color: "has-text-link",
+    display: document.querySelector("#score2Display"),
+    name: "Player Two",
+    score: 0,
+};
+
+const resetbtn = document.querySelector("#reset");
+const victoryCond = document.querySelector("#playto");
+const resultingWinner = document.querySelector("#winner");
 
 let endScore = parseInt(victoryCond.value);
 let gameOver = false;
 
-player1btn.addEventListener("click", () => {
-    if (!gameOver) {
-        player1Score++;
-        if (player1Score === endScore) {
-            gameOver = true;
-            player1Display.classList.add("winner");
-            player2Display.classList.add("loser");
-        }
-        player1Display.textContent = player1Score;
-    }
+player1.button.addEventListener("click", function () {
+    updateScore(player1, player2)
 });
 
-player2btn.addEventListener("click", () => {
-    if (!gameOver) {
-        player2Score++;
-        if (player2Score === endScore) {
-            gameOver = true;
-            player2Display.classList.add("winner");
-            player1Display.classList.add("loser");
-        }
-        player2Display.textContent = player2Score;
-    }
+player2.button.addEventListener("click", function () {
+    updateScore(player2, player1)
 });
 
 victoryCond.addEventListener("change", () => {
     if (parseInt(victoryCond.value) > endScore) {
         gameOver = false;
         endScore = parseInt(victoryCond.value);
-        resetDisplayColors();
+        hideWinner()
     }
 })
 
+function updateScore(scoringPlayer, rival) {
+    if (!gameOver) {
+        scoringPlayer.score++;
+        if (scoringPlayer.score === endScore) {
+            gameOver = true;
+            scoringPlayer.display.classList.add("has-text-success");
+            rival.display.classList.add("has-text-danger");
+            announceWinner(scoringPlayer);
+        }
+        scoringPlayer.display.textContent = scoringPlayer.score;
+    }
+}
+
 resetbtn.addEventListener("click", () => {
-    player1Score = player2Score = 0;
-    player1Display.textContent = player1Score;
-    player2Display.textContent = player2Score;
-    resetDisplayColors();
+    player1.score = player2.score = 0;
+    player1.display.textContent = player1.score;
+    player2.display.textContent = player2.score;
     gameOver = false;
+    endScore = parseInt(victoryCond.value);
+    hideWinner()
 })
 
 function resetDisplayColors() {
-    player1Display.classList.remove("winner", "loser");
-    player2Display.classList.remove("winner", "loser");
+    player1.display.classList.remove("has-text-success", "has-text-danger");
+    player2.display.classList.remove("has-text-success", "has-text-danger");
+}
+
+function disableButtons() {
+    player1.button.setAttribute("disabled", "");
+    player2.button.setAttribute("disabled", "");
+}
+
+function enableButtons() {
+    player1.button.removeAttribute("disabled");
+    player2.button.removeAttribute("disabled");
+}
+
+function announceWinner(winner) {
+    resultingWinner.textContent = `${winner.name} wins!`;
+    resultingWinner.classList.add(winner.color);
+    disableButtons()
+}
+
+function hideWinner() {
+    resultingWinner.classList.remove("has-text-success", "has-text-link");
+    resultingWinner.textContent = null;
+    enableButtons();
+    resetDisplayColors();
 }
